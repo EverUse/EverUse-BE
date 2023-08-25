d05fbf2c6080939662f1f4bc2c1273b46469e58a
Jimmy Jurado <jurado140@gmail.com>
2023-08-25 17:34:31 -0400

    generated rails secret key and added secret yml file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.assets.compile = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

2d166f120b0000f60690b49eeccf63ecb0e2cbad
Alex Kiselich <alexander.kiselich@gmail.com>
2023-08-26 10:11:38 -0600

    Circle CI fix try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
 
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.compile = false
   # Code is not reloaded between requests.
   config.cache_classes = true
 

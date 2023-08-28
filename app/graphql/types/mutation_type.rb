ae090b2b84d97c320e0994ff4a99eb29b3bf80bc
Jimmy Jurado <jurado140@gmail.com>
2023-08-28 17:03:08 -0400

    Removed unused code
    Co-authored-by: Ashley Plasket <aplasket@users.noreply.github.com>
    Co-authored-by: Alexander Kiselich <AlexKiselich@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,9 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_order_form, mutation: Mutation::CreateOrderForm
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end

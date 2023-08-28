9067f55dd37abd845c1ace21c774dcf1324f10ae
Jimmy Jurado <jurado140@gmail.com>
2023-08-28 10:07:18 -0400

    Adds conditional to application file.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module EverUse
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.precompile -= ['graphiql/rails/application.css']
+    if Rails.env.development?
+      config.assets.precompile -= ['graphiql/rails/application.css']
+    end
   end
 end

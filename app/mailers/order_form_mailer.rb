c964295ce82f305bcd577fe1085244a5e98e175d
Alex Kiselich <alexander.kiselich@gmail.com>
2023-08-29 13:43:04 -0600

    Updated confirmation email temp

--- app/mailers/order_form_mailer.rb
+++ app/mailers/order_form_mailer.rb
@@ -3,7 +3,12 @@ class OrderFormMailer < ApplicationMailer
 
   def confirmation_email(customer, email, comment, total, products)
     @name = customer
+    @products = products
+    @total = total
+    @comment = comment
+
     mail(to: email,
+         bcc: 'everusecustomerservice@gmail.com',
         subject: 'Order Confirmation')
   end
 end

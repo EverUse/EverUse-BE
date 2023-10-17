b3d9035cf0c714aeace49d403e4d5c0f70ee6dc7
Ashley Plasket <ashley.plasket@gmail.com>
2023-10-17 13:21:38 -0400

    fix the vcr merge conflict and  remove cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,17 +74,12 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
-  VCR.configure do |config|
-    config.cassette_library_dir = "spec/cassettes"
-    config.hook_into :webmock
-    config.configure_rspec_metadata!
-    config.ignore_localhost = true
-  end
 end
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_localhost = true
   config.filter_sensitive_data('google_private_id_key') { ENV['google_private_id_key'] } # add these for all sensitive data
   config.filter_sensitive_data('google_private_key') { ENV['google_private_key'] } # add these for all sensitive data
   config.filter_sensitive_data('google_client_id') { ENV['google_client_id'] } # add these for all sensitive data
@@ -92,6 +87,6 @@ VCR.configure do |config|
   config.filter_sensitive_data('google_client_cert_url') { ENV['google_client_cert_url'] } # add these for all sensitive data
   config.filter_sensitive_data('spreadsheet_key') { ENV['spreadsheet_key'] } # add these for all sensitive data
   config.configure_rspec_metadata! #allows you to use shortcut :vcr
-  config.default_cassette_options = { :allow_playback_repeats => true } #optional
-  config.default_cassette_options = { re_record_interval: 7.days} #configure to your liking, if needed and applicable
+  config.default_cassette_options = { :allow_playback_repeats => true }
+  config.default_cassette_options = { re_record_interval: 7.days}
 end

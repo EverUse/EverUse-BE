74601aaae4586750f67e048684f7d8e1a25d4eec
Ashley Plasket <ashley.plasket@gmail.com>
2023-10-31 14:23:41 -0400

    expand tests for product query

--- spec/graphql/queries/products/get_product_spec.rb
+++ spec/graphql/queries/products/get_product_spec.rb
@@ -8,11 +8,28 @@ module Queries
         @product2 = create(:product, name: "bracelet")
         @product3 = create(:product, name: "jewelry")
       end
+
       it "returns individual product" do
         product_data = query_product
+
         expect(product_data.count).to eq(2)
         expect(product_data.first[:name]).to eq(@product1.name)
-
+        expect(product_data.first).to have_key(:id)
+        expect(product_data.first[:id]).to be_a(String)
+        expect(product_data.first).to have_key(:name)
+        expect(product_data.first[:name]).to be_a(String)
+        expect(product_data.first).to have_key(:category)
+        expect(product_data.first[:category]).to be_a(String)
+        expect(product_data.first).to have_key(:image)
+        expect(product_data.first[:image]).to be_a(String)
+        expect(product_data.first).to have_key(:description)
+        expect(product_data.first[:description]).to be_a(String)
+        expect(product_data.first).to have_key(:color)
+        expect(product_data.first[:color]).to be_a(String)
+        expect(product_data.first).to have_key(:quantity)
+        expect(product_data.first[:quantity]).to be_an(Integer)
+        expect(product_data.first).to have_key(:size)
+        expect(product_data.first[:size]).to be_a(String)
       end
     end
 

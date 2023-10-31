da15f54e82ccf49bbc7827387cec9af8466be78e
Ashley Plasket <ashley.plasket@gmail.com>
2023-10-31 14:22:59 -0400

    validates presence of neccesary info

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   serialize :products, JSON
+
+  validates_presence_of :name
+  validates_presence_of :category
+  validates_presence_of :description
+  validates_presence_of :image
+  validates_presence_of :price
+  validates_presence_of :size
+  validates_presence_of :color
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:image) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:size) }
+    it { should validate_presence_of(:color) }
+  end
 end
